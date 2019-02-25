6d87018ecbb0fdf3b1d26b8b9fbf9b9fabc70913
bin7096 <bin_id@163.com>
2019-02-25 17:19:18 +0800

    2019-02-25

--- pages/my_info/index.js
+++ pages/my_info/index.js
@@ -4,14 +4,26 @@ Page({
         head_img : '../res/img/class_freezefish.png',
         user_name : '张三丰',
         mobile : '189****5478',
-        tabWidth : 0,
-        tabHeight : 0
+        tab : 0,
+        tab_list : [
+            {name : '我的订单', img : '../res/img/add.png', uri : 'pages/my_order/index'},
+            {name : '我的评价', img : '../res/img/add.png', uri : 'pages/my_eval/index'},
+            {name : '我的地址', img : '../res/img/add.png', uri : 'pages/my_addr/index'},
+            {name : '积分兑换', img : '../res/img/add.png', uri : 'pages/exchange/index'},
+            {name : '签到有惊喜', img : '../res/img/add.png', uri : 'pages/sign/index'},
+            {name : '退款/售后', img : '../res/img/add.png', uri : 'pages/afterSale'},
+            {name : '客服反馈', img : '../res/img/add.png', uri : 'pages/service/index'},
+            {name : '帮助中心', img : '../res/img/add.png', uri : 'pages/help/index'},
+        ]
     },
     onLoad : function () {
         var tabSize = common.getTabSize(4, true);
         this.setData({
-            tabWidth  : tabSize.width,
-            tabHeight : tabSize.height
+            tab  : tabSize.width,
         });
+    },
+    jumpTo : function (event) {
+        var uri = event.currentTarget.dataset.uri;
+        console.log(uri);
     }
 })
