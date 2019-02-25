a320720d9ccdff32691375b665f5b3c17b2f8197
bin7096 <bin_id@163.com>
2019-02-25 18:07:33 +0800

    2019-02-25

--- pages/my_info/index.js
+++ pages/my_info/index.js
@@ -6,14 +6,14 @@ Page({
         mobile : '189****5478',
         tab : 0,
         tab_list : [
-            {name : '我的订单', img : '../res/img/add.png', uri : 'pages/my_order/index'},
-            {name : '我的评价', img : '../res/img/add.png', uri : 'pages/my_eval/index'},
-            {name : '我的地址', img : '../res/img/add.png', uri : 'pages/my_addr/index'},
-            {name : '积分兑换', img : '../res/img/add.png', uri : 'pages/exchange/index'},
-            {name : '签到有惊喜', img : '../res/img/add.png', uri : 'pages/sign/index'},
-            {name : '退款/售后', img : '../res/img/add.png', uri : 'pages/afterSale'},
-            {name : '客服反馈', img : '../res/img/add.png', uri : 'pages/service/index'},
-            {name : '帮助中心', img : '../res/img/add.png', uri : 'pages/help/index'},
+            {name : '我的订单', img : '../res/img/add.png', uri : '../my_order/index'},
+            {name : '我的评价', img : '../res/img/add.png', uri : '../my_eval/index'},
+            {name : '我的地址', img : '../res/img/add.png', uri : '../my_addr/index'},
+            {name : '积分兑换', img : '../res/img/add.png', uri : '../exchange/index'},
+            {name : '签到有惊喜', img : '../res/img/add.png', uri : '../sign/index'},
+            {name : '退款/售后', img : '../res/img/add.png', uri : '../afterSale'},
+            {name : '客服反馈', img : '../res/img/add.png', uri : '../service/index'},
+            {name : '帮助中心', img : '../res/img/add.png', uri : '../help/index'},
         ]
     },
     onLoad : function () {
@@ -25,5 +25,14 @@ Page({
     jumpTo : function (event) {
         var uri = event.currentTarget.dataset.uri;
         console.log(uri);
+        wx.navigateTo({
+            url : uri,
+            success : function (res) {
+                console.log('success');
+            },
+            fail : function (err) {
+                console.log('failed');
+            }
+        });
     }
 })
