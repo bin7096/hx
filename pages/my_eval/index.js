8b5ef65382006c5b8ebf541bde116115ab4d5cb5
bin7096 <bin_id@163.com>
2019-02-27 22:47:49 +0800

    2019-02-27

--- pages/my_eval/index.js
+++ pages/my_eval/index.js
@@ -1,7 +1,7 @@
 Page({
     data : {
         orders : [
-            {id : 0, order_no : '4648648564856', status : '1', create_time : '01-06 15:28', goods : [
+            {id : 0, order_no : '4648648564856', status : '5', create_time : '01-06 15:28', goods : [
                 {id : 0, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 1, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 2, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
@@ -9,7 +9,7 @@ Page({
                 {id : 4, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 5, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
             ]},
-            {id : 1, order_no : '4648648564856', status : '2', create_time : '01-06 15:28', goods : [
+            {id : 1, order_no : '4648648564856', status : '5', create_time : '01-06 15:28', goods : [
                 {id : 0, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 1, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 2, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
@@ -17,7 +17,7 @@ Page({
                 {id : 4, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 5, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
             ]},
-            {id : 2, order_no : '4648648564856', status : '3', create_time : '01-06 15:28', goods : [
+            {id : 2, order_no : '4648648564856', status : '4', create_time : '01-06 15:28', goods : [
                 {id : 0, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1},
                 {id : 1, name : '智利原味三文鱼排', img : '../res/img/class_seafish.png', price : '20.1', number : 1}
             ]},
